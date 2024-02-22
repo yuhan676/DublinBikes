428270664f7ea1233b34113a42b5463da126d6a8
yuhan676 <yuhanhu676@gmail.com>
2024-02-22 19:57:21 +0000

    Removed the schema-creation code to a separate file, removed the login credential to a db_config file, changed connect_db.py file name to create_db.py

---
+++ create_db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+from db_config_Yuhan import db_type, username,password,hostname,port,default_db
+
+
+# Create an engine to connect to the default database
+engine = create_engine(f'{db_type}://{username}:{password}@{hostname}:{port}/{default_db}')
+
+try:
+    # Obtain a connection from the engine
+    with engine.connect() as connection:
+        # Check if the database already exists
+        db_exists = connection.execute(
+            text("SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = :db_name"),
+            {'db_name': 'dublinbike_db'}
+        ).scalar()
+        if not db_exists:
+            # Database does not exist, create it
+            connection.execute(text("CREATE DATABASE dublinbike_db"))
+            print("Database 'DublinBike_db' created successfully")
+        else:
+            print("Database 'DublinBike_db' already exists")
+
+    # Continue with the rest of your code...
+except SQLAlchemyError as e:
+    print(f"An error occurred: {e}")

5f73bbc4b980a2e5e56b5af9882353e87e270177
RiinKal <riinkaljurand@gmail.com>
2024-02-23 12:34:52 -0800

    Uploading schemas, and config file

---
+++ Code/Database/Riin_database/winfo_config.py
@@ -0,0 +1,7 @@
+db_type = 'mysql' # change to mysql
+username = 'admin' 
+password = '%November2022'
+hostname = 'db-test.cfm2usmq8eoy.eu-north-1.rds.amazonaws.com'
+port = '3306' # port 3306 for mysql
+default_db = 'mysql'
+db_name = dublinbike_db

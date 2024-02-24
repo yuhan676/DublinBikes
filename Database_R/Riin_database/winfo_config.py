198fe9aedabfe6141aefc82fb0394ea9cdd8a6c9
RiinKal <157484304+RiinKal@users.noreply.github.com>
2024-02-23 16:08:14 -0800

    Update winfo_config.py
    syntax error

--- Database_R/Riin_database/winfo_config.py
+++ Database_R/Riin_database/winfo_config.py
@@ -4,4 +4,4 @@ password = '%November2022'
 hostname = 'db-test.cfm2usmq8eoy.eu-north-1.rds.amazonaws.com'
 port = '3306' # port 3306 for mysql
 default_db = 'mysql'
-db_name = dublinbike_db
+db_name = 'dublinbike_db'

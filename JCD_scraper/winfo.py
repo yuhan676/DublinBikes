f04026a2ad0dd474632c8c0ed152cdd57e232777
RiinKal <riinkaljurand@gmail.com>
2024-02-28 05:58:17 -0800

    Update links

--- JCD_scraper/winfo.py
+++ JCD_scraper/winfo.py
@@ -4,11 +4,11 @@
 #     return "daf15fe5d6b088b90171f21de62933df"
 
 # def URL1():
-#     return "https://api.openweathermap.org/data/2.5/weather"
+#     return "https://api.openweathermap.org/data/2.5/weather?q=dublin"
 
 # def URL2():
-#     return "https://api.openweathermap.org/data/2.5/forecast"
+#     return "https://api.openweathermap.org/data/2.5/forecast?q=dublin"
 
 API_KEY = "daf15fe5d6b088b90171f21de62933df"
-URL1 = "https://api.openweathermap.org/data/2.5/weather"
-URL2 = "https://api.openweathermap.org/data/2.5/forecast"
+URL1 = "https://api.openweathermap.org/data/2.5/weather?q=dublin"
+URL2 = "https://api.openweathermap.org/data/2.5/forecast?q=dublin"

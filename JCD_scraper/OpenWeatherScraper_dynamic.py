86462ce762fa7d91367ed1f69669df8e85ba6574
yuhan676 <yuhanhu676@gmail.com>
2024-02-28 13:26:42 +0000

    defined api key and URL directly in scraper code

--- JCD_scraper/OpenWeatherScraper_dynamic.py
+++ JCD_scraper/OpenWeatherScraper_dynamic.py
@@ -1,13 +1,16 @@
 import requests
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
-from winfo import API_KEY, URL1, URL2
+# from winfo import API_KEY, URL1, URL2
 from db_config import db_type, username, password, hostname, port, db_name
 import traceback
 import datetime
 
 engine = create_engine(f'{db_type}://{username}:{password}@{hostname}:{port}/{db_name}')
 
+API_KEY = "daf15fe5d6b088b90171f21de62933df"
+URL1 = "https://api.openweathermap.org/data/2.5/weather"
+URL2 = "https://api.openweathermap.org/data/2.5/forecast"
 # function that inserts values to current weather table
 def insert_current_weather():
     try:

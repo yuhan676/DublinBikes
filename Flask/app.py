6664b59e4486e4e195d0d6d121fdd3b70668843a
yuhan676 <yuhanhu676@gmail.com>
2024-03-03 14:53:42 +0000

    added port specification to app.py to resolve the 'Address already in use' issue

--- Flask/app.py
+++ Flask/app.py
@@ -119,8 +119,7 @@ def internal_server_error(error):
 """
 
 if __name__ == '__main__':
-    app.run(debug=True)
-#, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 # create a test function to test the functionality of the web page and various queries
 # app.run(debug=True)

e65c61ccaccb4654414f6d3f090b240e330147c8
RiinKal <riinkaljurand@gmail.com>
2024-03-05 15:38:25 -0800

    update

--- Flask/tests.py
+++ Flask/tests.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 from flask import request, jsonify
-from functions import connect_db, get_station_names, suggest_stations
+from functions import connect_db, get_station_names
+from app import suggest_stations
 import traceback
 import unittest
 from unittest.mock import MagicMock, patch

6a3734b9512d75a2a73d045628dcab1a1ec25a65
RiinKal <riinkaljurand@gmail.com>
2024-03-16 12:12:50 -0700

    update

--- Flask/static/map.js
+++ Flask/static/map.js
@@ -1,6 +1,14 @@
-function initMap() {
+/*function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
+    });
+}
+*/
+function initDublin() {
+    var dublin = { lat: 53.349805, lng: -6.26031 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: dublin
     });
 }

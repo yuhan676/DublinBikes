4f2f70c086f9a36e4040083bdc0936f0e2c74a17
RiinKal <riinkaljurand@gmail.com>
2024-03-23 15:40:27 -0700

    update

--- Flask/current_weather.py
+++ Flask/current_weather.py
@@ -2,13 +2,13 @@ from functions import save_weather_to_json, fetch_weather_data
 
 def main():
     try:
-        # Connect to the weather API
+        # Connect to the databse
         weather_data = fetch_weather_data()
         
         # Save the weather data to a JSON file
         save_weather_to_json(weather_data, 'weather_data.json')
         print("Weather data saved to weather_data.json")
-
+    # error exception
     except Exception as e:
         print("Error fetching or saving weather data:", e)
 

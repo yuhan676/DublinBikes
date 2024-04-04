befc03dbfcfea007122e24a2de2e1887e3ea09c3
RiinKal <riinkaljurand@gmail.com>
2024-04-04 13:53:13 -0700

    test

--- Flask/static/index.js
+++ Flask/static/index.js
@@ -452,14 +452,14 @@ function selectStation(index, isRent) {
     populateRightPanel(stationName, isRent);
 
     // Load the Visualization API and the corechart package.
-    // google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {'packages':['corechart']});
 
     // Set a callback to run when the Google Visualization API is loaded.
-    // google.charts.setOnLoadCallback(function() {
+    google.charts.setOnLoadCallback(function() {
         // Call generatePredictionGraphs with stationName
-    generatePredictionGraphs(stationName);
-        //});
-    }
+        generatePredictionGraphs(stationName);
+    });
+}
 // Right hand Panel function to populate station and bike data
 function populateRightPanel(stationName, isRent) {
     try {

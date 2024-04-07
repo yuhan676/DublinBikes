5b0a4e71faa7e4dd7f6102ff203ac04737333954
RiinKal <riinkaljurand@gmail.com>
2024-04-07 11:53:29 -0700

    test

--- Flask/static/index.js
+++ Flask/static/index.js
@@ -430,8 +430,8 @@ function populateRightPanel(stationName, isRent) {
             
             // Initialize the data table
             var hourlyBikeData = new google.visualization.DataTable();
-            hourlyBikeData.addColumn('number', 'Hour');
-            hourlyBikeData.addColumn('number', 'Bikes');
+            hourlyBikeData.addColumn('number', stationData.time_update);
+            hourlyBikeData.addColumn('number', stationData.total_bikes);
      
             // Manually add rows one by one
             for (var hour = 0; hour < 24; hour++) {

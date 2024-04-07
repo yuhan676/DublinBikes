b933c1a6748a1a468483410fa8bb4593b8d56674
RiinKal <riinkaljurand@gmail.com>
2024-04-07 11:51:13 -0700

    test

--- Flask/static/index.js
+++ Flask/static/index.js
@@ -443,6 +443,7 @@ function populateRightPanel(stationName, isRent) {
                 legend: { position: 'none' },
                 hAxis: {
                     title: 'Hour',
+                    format: '0',
                     ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                 }
             };

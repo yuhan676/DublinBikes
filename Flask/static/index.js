6492f7e95794316737bf789ce8d3f6207b5eb2ba
RiinKal <riinkaljurand@gmail.com>
2024-04-08 19:28:09 +0100

    test

--- Flask/static/index.js
+++ Flask/static/index.js
@@ -506,8 +506,8 @@ function populateRightPanel(stationName, isRent) {
                     maxValue: 25 // Set the maximum value for the vertical axis
                 },
                 legend: { position: 'none' },
-                width: 150, // Set the width of the chart
-                height: 150 // Set the height of the chart
+                width: 350, // Set the width of the chart
+                height: 350 // Set the height of the chart
             }; 
             
             // Create container for the chart

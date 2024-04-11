75452b99c270d50218819a64c118c6dae55f2def
yuhan676 <yuhanhu676@gmail.com>
2024-04-11 12:38:39 +0100

    Test changing icon

--- Flask/static/map.js
+++ Flask/static/map.js
@@ -154,7 +154,7 @@ function addUserLocationMarker(position) {
         map: map,
         position: userLocation,
         icon: {
-          url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', // The URL to your custom icon
+          url: 'http://maps.gstatic.com/mapfiles/ms2/micons/man.png', // The URL to your custom icon
           // set desired size and anchor settings here if necessary
         }
       });

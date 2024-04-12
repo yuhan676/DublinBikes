267f6fda02eed14c903d099572e0ea41974c49cc
RiinKal <157484304+RiinKal@users.noreply.github.com>
2024-04-12 10:26:18 +0100

    Update db_data_request.py
    changed query code

--- Flask/predictive_models/db_data_request.py
+++ Flask/predictive_models/db_data_request.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
+# Database connection details
 db_type = 'mysql'  # Change to 'mysql' for MySQL/MariaDB
 username = 'admin'
 password = 'DublinBike2024%'
@@ -27,19 +28,11 @@ try:
         ) AS union_query
         """
 
-        # Query to retrieve last_update timestamps from station and station_status tables
-        sql_last_update_station = "SELECT last_update FROM station LIMIT 1"
-        sql_last_update_station_status = "SELECT last_update FROM station_status LIMIT 1"
-
-        # Execute the queries
+        # Execute the query
         result_time_update_weather = connection.execute(text(sql_time_update_weather)).fetchall()
-        result_last_update_station = connection.execute(text(sql_last_update_station)).fetchone()
-        result_last_update_station_status = connection.execute(text(sql_last_update_station_status)).fetchone()
 
         # Extract timestamps
         timestamps_weather = [result[0] for result in result_time_update_weather]
-        last_update_station = result_last_update_station[0] if result_last_update_station else None
-        last_update_station_status = result_last_update_station_status[0] if result_last_update_station_status else None
 
 except SQLAlchemyError as e:
     print("An error occurred:", e)

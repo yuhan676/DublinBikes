4e89f36089dbaa7998e039a7849fd968fd703d84
yuhan676 <yuhanhu676@gmail.com>
2024-04-17 12:56:13 +0100

    changed the OW url I can find to have the correct longitude and lattitude. using q=dublin resulted in scraping Dublin Ohio weather

--- Prototype_scrapers/winfo.py
+++ Prototype_scrapers/winfo.py
@@ -4,7 +4,7 @@ def API_KEY():
     return "daf15fe5d6b088b90171f21de62933df"
 
 def URL1():
-    return "https://api.openweathermap.org/data/2.5/weather?q=dublin"
+    return "https://api.openweathermap.org/data/2.5/weather?lat=53.350140&lon=-6.266155"
 
 def URL2():
-    return "https://api.openweathermap.org/data/2.5/forecast?q=dublin"
+    return "https://api.openweathermap.org/data/2.5/lat=53.350140&lon=-6.266155"
